2ff8503c5a213eee01a8f9cd647cd226834bebbe
Vijaya Lakshmi Kocherla <vl.kocherla@gmail.com>
2026-01-24 14:33:46 -0800

    Fix frontend fetch to use relative path for EC2 deployment

--- public/js/app.js
+++ public/js/app.js
@@ -11,16 +11,27 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error
-          return
-        }
-        messageOne.textContent = data.location
-        messageTwo.textContent = data.forecast
-      })
-    }
-  )
+  // fetch(`http://localhost:3000/weather?address=${location}`).then(
+  //   (response) => {
+  //     response.json().then((data) => {
+  //       if (data.error) {
+  //         messageOne.textContent = data.error
+  //         return
+  //       }
+  //       messageOne.textContent = data.location
+  //       messageTwo.textContent = data.forecast
+  //     })
+  //   }
+  // )
+
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error
+        return
+      }
+      messageOne.textContent = data.location
+      messageTwo.textContent = data.forecast
+    })
+  })
 })
